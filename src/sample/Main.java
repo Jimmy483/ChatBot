cd47eea4f05fc2f9de3d7f4844c5ab9b1ec758b4
Jimmy Sherpa <jimmy483>
2022-01-09 19:57:37 +0545

    couldnt solve with bot message change line through dynamic way

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,12 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Main implements  ActionListener{
+public class Main implements  ActionListener {
     JButton btnSend=new JButton();
     TextField message=new TextField();
     TextArea textArea=new TextArea();
     Label title=new Label();
-    String[] arr={"Hi","Nice to meet you","I love you too"};
+    String[] arr={"Hi","Nice to meet you","I love you too","My name is Borat and i am from Kazakhstan. My neighbours are noisy and always try to copy me"};
 
     public static void main(String[] args) {
         new Main();
@@ -42,6 +42,9 @@ public class Main implements  ActionListener{
 
             textArea.setBounds(40,80,390,300);
             textArea.setEditable(false);
+            float a=textArea.getAlignmentX();
+            float b=textArea.getWidth();
+        System.out.println(a + "" +b);
 //            textArea.addKeyListener(new KeyListener() {
 //                @Override
 //                public void keyTyped(KeyEvent e) {
@@ -130,7 +133,7 @@ public class Main implements  ActionListener{
 
         message.setText("");
         Random rn=new Random();
-        int num=rn.nextInt(3);
+        int num=rn.nextInt(4);
         //aligning bot message to right
         for(int i=0;i<60;i++){
             textArea.append(" ");
@@ -142,7 +145,32 @@ public class Main implements  ActionListener{
                 textArea.append(" ");
             }
         }
-        textArea.append(arr[num] + "\n");
+
+        int leng=arr[num].length();
+//        for(int i=0;i<leng;i++){
+//
+//            textArea.append(arr[num].substring(i,i+1));
+//            System.out.println(textArea.getCursor().toString());
+//
+//        }
+//        int changeWidth=textArea.getWidth()/2;
+//        boolean check=true;
+//
+//        if(leng*30>changeWidth){
+//            while(leng*30>=changeWidth){
+//                textArea.append(arr[num].substring(0,changeWidth/30));
+//                arr[num]=arr[num].substring(changeWidth/30,leng);
+//                if(arr[num].length()<changeWidth/30){
+//                    textArea.append(arr[num]);
+//                }
+//                leng=leng-(int)(changeWidth/30);
+//
+//            }
+//            textArea.append("\n");
+//        }
+
+        //textArea.append("\n");
+        //textArea.append(arr[num] + "\n");
     }
 
     @Override
